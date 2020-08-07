ab2a22979ff7726e858b71deb2a38bf614d41923
Alireza Ebadi Ghajari <alirezaebg@gmail.com>
2020-08-07 10:07:13 -0700

    Change evaluateAnswer to reular function and bind it to this in the Game constructor

--- src/Game.js
+++ src/Game.js
@@ -11,6 +11,7 @@ class Game extends Component {
             value3: valuesArray[2],
             proposedAnswer: valuesArray[3],
         };
+        this.handleAnswer = this.handleAnswer.bind(this);
     }
 
     makeNewQuestion = () => {
@@ -21,7 +22,7 @@ class Game extends Component {
         return [value1, value2, value3, proposedAnswer];
     };
 
-    handleAnswer = event => {
+    handleAnswer(event) {
         const newValuesArray = this.makeNewQuestion();
         this.updateState(newValuesArray);
         const answerWasCorrect = this.evaluateAnswer(event.target.name);
